4de16c3bcb0803ebd3f7b87008e8a77386b6cd87
andresusanto <andre@andresusanto.info>
2014-11-29 23:19:11 +0700

    update server

--- SERVER/s_reg.php
+++ SERVER/s_reg.php
@@ -17,17 +17,28 @@ switch ($metode){
 						$token = rand(1234, 9999);
 						
 						if (db_execute("INSERT INTO `user` (`username`, `password`, `no_hp`, `reg_token`) VALUES ('$uname', '$pw', '$hp', '$token');")){
-							echo 'S ' . $token;
+							echo 'S'; // . $token;
 						}else{
 							echo 'E';
 						}
 					}
+					break;
+					
+	case 'A'	:	$uname 	= req_handler('uname');
+					$token 	= req_handler('token');
+					if (db_num("SELECT `username` FROM `user` WHERE `username`='$uname' AND reg_token = '$token'") > 0){
+						db_execute("UPDATE `user` SET reg_token='1' WHERE `username`='$uname'");
+						echo 'S';
+					}else{
+						echo 'E';
+					}
 					
 					break;
+					
 	case 'L'	:	$uname 	= req_handler('uname');
 					$pw 	= req_handler('pass');
 					
-					if (db_num("SELECT `username` FROM `user` WHERE `username`='$uname'") > 0){
+					if (db_num("SELECT `username` FROM `user` WHERE `username`='$uname' AND reg_token = '1'") > 0){
 						$_SESSION['user'] = $uname;
 						echo 'OK';
 					}else{
