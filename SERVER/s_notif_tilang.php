87f6501bf3f7ef02101d3094ba7464cfc3cab117
Tony <13512018@std.stei.itb.ac.id>
2014-11-30 09:43:00 +0700

    add s_show_surat.php

--- SERVER/s_notif_tilang.php
+++ SERVER/s_notif_tilang.php
@@ -9,9 +9,9 @@ $uname = req_handler('uname');
 //DT : Show Detil Tilang
 switch($metode)
 {
-	case 'DT' : if (db_num("SELECT `username` FROM `user` WHERE `username` = '$uname'") == 1)
+	case 'LT' : if (db_num("SELECT `username` FROM `user` WHERE `username` = '$uname'") == 1)
 				{
-					$data_post = db_fetchs("SELECT `stnk`.`jenis`,`surat_tilang`.*,`pelanggaran`.`jenis_pelanggaran`,`pelanggaran`.`deskripsi_pelanggaran`,`pelanggaran`.`biaya_denda`,`polisi`.`nama` FROM `user_stnk`,`stnk`,`surat_tilang`,`pelanggaran`,`polisi` WHERE `user_stnk`.`username` = '$uname' AND `stnk`.`no_stnk` = `user_stnk`.`no_stnk` AND `stnk`.`no_stnk` = `surat_tilang`.`no_stnk` AND `surat_tilang`.`pasal_pelanggaran` = `pelanggaran`.`pasal_pelanggaran` AND `polisi`.`no_anggota` = `surat_tilang`.`no_tilang`");
+					$data_post = db_fetchs("SELECT `stnk`.`jenis`,`surat_tilang`.*,`pelanggaran`.`jenis_pelanggaran`,`pelanggaran`.`deskripsi_pelanggaran`,`pelanggaran`.`biaya_denda`,`polisi`.`nama` FROM `user_stnk`,`stnk`,`surat_tilang`,`pelanggaran`,`polisi` WHERE `user_stnk`.`username` = '$uname' AND `stnk`.`no_stnk` = `user_stnk`.`no_stnk` AND `stnk`.`no_stnk` = `surat_tilang`.`no_stnk` AND `surat_tilang`.`pasal_pelanggaran` = `pelanggaran`.`pasal_pelanggaran` AND `polisi`.`no_anggota` = `surat_tilang`.`no_anggota`");
 					if ($data_post  == 0)
 					{
 						echo json_encode(null); // Data Tilang Not Found
@@ -19,24 +19,22 @@ switch($metode)
 					else
 					{
 						for ($i=1;$i<=$data_post[0];$i++)
-						{
 							$dt[$i-1] = $data_post[$i];
-						}
 						echo json_encode($dt);
 					}
 				}
 				else
 				{
-					echo json_enconde(null); // User not found
+					echo json_encode(null); // User not found
 				}
 				break;
 				
-	case 'LT' : $no_tilang = req_handler('no_tilang');
+	case 'DT' : $no_tilang = req_handler('no_tilang');
 				if (db_num("SELECT `username` FROM `user` WHERE `username` = '$uname'") == 1)
 				{
 					if (db_num("SELECT `no_tilang` FROM `surat_tilang` WHERE `no_tilang` = '$no_tilang'") == 1)
 					{
-						$data_post = db_fetchs("SELECT `stnk`.`jenis`,`surat_tilang`.*,`pelanggaran`.`jenis_pelanggaran`,`pelanggaran`.`deskripsi_pelanggaran`,`pelanggaran`.`biaya_denda`,`polisi`.`nama` FROM `user_stnk`,`stnk`,`surat_tilang`,`pelanggaran`,`polisi` WHERE `user_stnk`.`username` = '$uname' AND `stnk`.`no_stnk` = `user_stnk`.`no_stnk` AND `stnk`.`no_stnk` = `surat_tilang`.`no_stnk` AND `surat_tilang`.`pasal_pelanggaran` = `pelanggaran`.`pasal_pelanggaran` AND `polisi`.`no_anggota` = `surat_tilang`.`no_tilang` AND `surat_tilang`.`no_tilang` = '$no_tilang'");
+						$data_post = db_fetchs("SELECT `stnk`.`jenis`,`surat_tilang`.*,`pelanggaran`.`jenis_pelanggaran`,`pelanggaran`.`deskripsi_pelanggaran`,`pelanggaran`.`biaya_denda`,`polisi`.`nama` FROM `user_stnk`,`stnk`,`surat_tilang`,`pelanggaran`,`polisi` WHERE `user_stnk`.`username` = '$uname' AND `stnk`.`no_stnk` = `user_stnk`.`no_stnk` AND `stnk`.`no_stnk` = `surat_tilang`.`no_stnk` AND `surat_tilang`.`pasal_pelanggaran` = `pelanggaran`.`pasal_pelanggaran` AND `polisi`.`no_anggota` = `surat_tilang`.`no_anggota` AND `surat_tilang`.`no_tilang` = '$no_tilang'");
 						if ($data_post  == 0)
 						{
 							echo json_encode(null); // Data Tilang Not Found
@@ -44,23 +42,21 @@ switch($metode)
 						else
 						{
 							for ($i=1;$i<=$data_post[0];$i++)
-							{
 								$dt[$i-1] = $data_post[$i];
-							}
 							echo json_encode($dt);
 						}
 					}
 					else
 					{
-						echo json_enconde(null);
+						echo json_encode(null);
 					}
 					
 				}
 				else
 				{
-					echo json_enconde(null);
+					echo json_encode(null);
 				}
 				break;
-	default :	echo json_enconde(null);
+	default :	echo json_encode(null);
 }
 ?>
